6411e7a91c5ad256f87d3184ae66a183c2d55c9b
Die <growder1@gmail.com>
2025-06-04 15:06:37 +0500

    Обновил Миграцию обратной связи

--- src/main/resources/db/migration/V2__Create_feedback.sql
+++ src/main/resources/db/migration/V2__Create_feedback.sql
@@ -1,6 +1,6 @@
 CREATE TABLE feedback (
                           id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-                          rating INT NOT NULL CHECK (rating BETWEEN 1 AND 5),
+                          email VARCHAR(1000) NOT NULL,
                           comment VARCHAR(1000) NOT NULL,
                           created_at TIMESTAMP NOT NULL
 );
